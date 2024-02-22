fefa37f7e3397e35a8973fdc4767ce501c51026d
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2024-02-22 10:28:07 +0800

    secondary navbar functionality complete

--- src/components/SecondaryNav.jsx
+++ src/components/SecondaryNav.jsx
@@ -5,17 +5,31 @@ import { useAppDispatch } from "../store/hooks";
 import {setTag} from "../store/slices/appSlice";
 import { Link } from "react-router-dom";
 
+
+
 export default function SecondaryNav() {
     const dispatch = useAppDispatch();
     const [active, setActive] = useState("");
 
+    const handleClick = (tag) => {
+        dispatch(setTag(tag));
+        console.log(tag);
+    }
+
     return (
-        <nav className={`${styles.paddingX} w-full flex items-center py-8 border-b-2 border-slate-200 fixed top-24 border-t-2 z-20 bg-white`}>
+        <nav className={`${styles.paddingX} w-full flex items-center py-4 border-b-2 border-slate-200 fixed top-24 border-t-2 z-20 bg-white`}>
+            
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
                 <ul className="list-none hidden sm:flex flex-row gap-10">
                     {secondaryNavLinks.map((link) => (
-                        <li key={link.id}>
-                                <p className="font-playfair font-medium">
+                        <li key={link.id} onClick={() => {setActive(link.id); handleClick(link.id)}}
+                        className={`${
+                            active === link.title
+                            ? 'text-slate-500'
+                            : 'text-black'
+                        }`}
+                        >
+                                <p className="font-playfair cursor-pointer hover:text-slate-500 font-medium">
                                 {link.title}
                                 </p>
                         </li>
