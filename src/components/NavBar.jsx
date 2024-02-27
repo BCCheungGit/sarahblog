3aac6e239bef1da1b4f73c8ad65e807cefe9a6da
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2024-02-27 15:10:10 +0800

    updated file structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, { useState} from "react";
 
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import collapse from "../assets/collapsenav.svg";
 import { motion } from "framer-motion";
-import { fadeIn } from "../utils/motion";
 
 export default function NavBar() {
     const [toggle, setToggle] = useState(false);
@@ -51,12 +50,12 @@ export default function NavBar() {
                             className="cursor-pointer w-[31px] h-[31px] object-contain"
                             onClick={() => setToggle(!toggle)}
                         />
-                        <motion.div
-                        initial = "hidden"
-                        animate = "show"
-                        variants = {fadeIn("down", "tween", 0, 0.6)}
-                        >
-                        <div className={`${!toggle ? 'hidden' : 'flex'} p-6 absolute bg-white top-20 right-0 mx-0 my-2 min-w[140px] z-10 border-slate-200 border-2`}>
+                        <motion.div 
+                        initial={{ opacity: 0, y: -20 }}  // Initial state of the menu (hidden and moved to the left)
+                        animate={{ opacity: toggle ? 1 : 0, y: toggle ? 0 : -20 }}  // Animate opacity and x position based on the toggle state
+                        transition={{ duration: 0.5 }} 
+                        className={`${!toggle ? 'hidden' : 'flex'} p-6 absolute bg-white top-20 right-0 mx-0 my-2 min-w[140px] z-10 border-slate-200 border-2`}>
+                            
                             <ul className="list-none flex justify-end items-start flex-col gap-2">
                                 <li>
                                     <Link
@@ -78,8 +77,8 @@ export default function NavBar() {
                                 ))}
 
                             </ul>
-                        </div>
                         </motion.div>
+                        
                     </div>
                     
                 </div>
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,14 +1,81 @@
-import ContactForm from "../ContactForm";
-
-
+import React, {useState, useRef} from 'react';
+import {motion} from 'framer-motion';
+import { slideIn } from '../../utils/motion';
+import { fadeIn } from '../../utils/motion';
 
 export default function Contact() {
-
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        message: '',
+    })
+    const formRef = useRef();
+    const handleChange = (e) => {
+        const { name, value} = e.target;
+    
+        setForm({...form, [name]: value})
+    
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submitted!");
+    }
 
     return (
-    <div className="flex flex-col gap-8">
-        <ContactForm />
-        
-    </div>
+        <>
+            <div className='xl:mt-12 xl:top-20 justify-center top-24 w-screen h-auto absolute xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+            <motion.div
+               initial= "hidden"
+                animate="show"
+                variants={fadeIn("left", "tween", 0, 0.5)}
+                className='flex flex-col p-8 rounded-sm border-t-2 border-b-2 border-slate-300 justify-center items-center w-3/5'
+            >
+                    <h3 className='font-playfair text-2xl'>CONTACT</h3>
+                <form
+                    ref = {formRef}
+                    onSubmit = {handleSubmit}
+                    className='mt-6 flex flex-col'
+                >
+                    <label className='flex flex-col'>
+                        <span className='font-playfair font-medium mr-4 mt-4'>Your Name: </span>
+                        <input 
+                            type = "text"
+                            name = "name"
+                            value = {form.name}
+                            onChange = {handleChange}
+                            placeholder = "What's your name?"
+                            className='py-2 px-6 border-2 mt-2 font-medium font-playfair placeholder:text-slate-300'
+                        />
+                    </label>
+                    <label className='flex flex-col'>
+                        <span className='font-playfair font-medium mr-4 mt-4'>Your Email: </span>
+                        <input 
+                            type="email"
+                            name="email"
+                            value = {form.email}
+                            onChange={handleChange}
+                            placeholder="What's your email?"
+                            className='py-2 px-6 border-2 mt-2 font-medium font-playfair placeholder:text-slate-300'
+                        />
+
+                    </label>
+                    <label className='flex flex-col'>
+                        <span className='font-playfair font-medium mr-4 mt-4'>Your Message: </span>
+                        <textarea
+                            name="message"
+                            value={form.message}
+                            onChange={handleChange}
+                            placeholder="What's your message?"
+                            className='py-2 h-24 px-6 border-2 mt-2 font-medium font-playfair placeholder:text-slate-300'
+                        />
+
+                    </label>
+
+                </form>
+                </motion.div>
+
+            </div>
+        </>
     )
 }
